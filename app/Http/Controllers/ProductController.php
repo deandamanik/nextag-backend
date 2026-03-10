80891c447709768501406d882cd7f4cc6e772cf8
deandamanik <deandamanik0706@gmail.com>
2026-03-10 21:30:40 +0800

    feat: implement ProductController and basic routing

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Product::latest()->get();
+        return view('products.index', compact('products'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'price' => 'required|numeric',
+            'stock' => 'required|numeric',
+        ]);
+
+        $productCode = 'NT-' . strtoupper(Str::random(8));
+
+        Product::create([
+            'name' => $request->name,
+            'product_code' => $productCode,
+            'description' => $request->description,
+            'price' => $request->price,
+            'stock' => $request->stock,
+        ]);
+
+        return redirect()->back()->with('success', 'Produk NexTag berhasil didaftarkan!');
+    }
 }
